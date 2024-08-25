9eeb36bf0840c916354203686016d36ce6806099
Alan Szmyt <szmyty@gmail.com>
2024-08-25 00:22:19 -0400

    Update.

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -17,19 +17,23 @@ module.exports = {
    */
 
   rules: {
-    "type-enum": [2, "always", [
-      "feat",     // A new feature
-      "fix",      // A bug fix
-      "docs",     // Documentation only changes
-      "style",    // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
-      "refactor", // A code change that neither fixes a bug nor adds a feature
-      "perf",     // A code change that improves performance
-      "test",     // Adding missing tests or correcting existing tests
-      "build",    // Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
-      "ci",       // Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
-      "chore",    // Other changes that don't modify src or test files
-      "revert"    // Reverts a previous commit
-    ]],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // A new feature
+        "fix", // A bug fix
+        "docs", // Documentation only changes
+        "style", // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+        "refactor", // A code change that neither fixes a bug nor adds a feature
+        "perf", // A code change that improves performance
+        "test", // Adding missing tests or correcting existing tests
+        "build", // Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+        "ci", // Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
+        "chore", // Other changes that don't modify src or test files
+        "revert", // Reverts a previous commit
+      ],
+    ],
     "subject-case": [2, "always", "sentence-case"],
     "subject-empty": [2, "never"],
     "subject-max-length": [2, "always", 50],
@@ -38,53 +42,56 @@ module.exports = {
     "scope-case": [2, "always", "lower-case"],
     "type-case": [2, "always", "lower-case"],
     "type-empty": [2, "never"],
-    "scope-empty": [0, "never"]
+    "scope-empty": [0, "never"],
   },
 
   types: {
     feat: {
       description: "A new feature",
-      title: "Features"
+      title: "Features",
     },
     fix: {
       description: "A bug fix",
-      title: "Bug Fixes"
+      title: "Bug Fixes",
     },
     docs: {
       description: "Documentation only changes",
-      title: "Documentation"
+      title: "Documentation",
     },
     style: {
-      description: "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
-      title: "Styles"
+      description:
+        "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
+      title: "Styles",
     },
     refactor: {
       description: "A code change that neither fixes a bug nor adds a feature",
-      title: "Code Refactoring"
+      title: "Code Refactoring",
     },
     perf: {
       description: "A code change that improves performance",
-      title: "Performance Improvements"
+      title: "Performance Improvements",
     },
     test: {
       description: "Adding missing tests or correcting existing tests",
-      title: "Tests"
+      title: "Tests",
     },
     build: {
-      description: "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
-      title: "Builds"
+      description:
+        "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
+      title: "Builds",
     },
     ci: {
-      description: "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)",
-      title: "Continuous Integration"
+      description:
+        "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)",
+      title: "Continuous Integration",
     },
     chore: {
       description: "Other changes that don't modify src or test files",
-      title: "Chores"
+      title: "Chores",
     },
     revert: {
       description: "Reverts a previous commit",
-      title: "Reverts"
-    }
-  }
+      title: "Reverts",
+    },
+  },
 };
